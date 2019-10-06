32385a9aad4f8b4957261a66167b1b0d00a7c0a2
tatia-kom <komarovats93@gmail.com>
2019-10-06 13:56:06 +0300

    start

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+    $('.tel-input').inputmask({
+        "mask": "+7 (999) 999-99-99"
+        , "placeholder": "_"
+        , showMaskOnHover: false
+        , showMaskOnFocus: true
+    });
+});
