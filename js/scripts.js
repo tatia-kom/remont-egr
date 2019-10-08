c9a476e4494a04b7a6abbe6edf22b696ac4036e4
tatia-kom <komarovats93@gmail.com>
2019-10-08 17:15:08 +0300

    feat: add scrolling header and some animations

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
 
+    $('.head__gifts-item').each(function (index) {
+        setTimeout(function() {
+            $('.head__gifts-item:nth-child('+(index + 1)+')').removeClass('head__gifts-item--hidden');
+        }, (index+1) * 300);
+    });
+
     $('.tel-input').inputmask({
         "mask": "+7 (999) 999-99-99"
         , "placeholder": "_"
@@ -10,5 +16,28 @@ $(document).ready(function() {
     $('.header__mobile-menu').click(function(e) {
         e.preventDefault();
         $('.header__menu').toggleClass('header__menu--opened');
+        if ($(window).scrollTop() == 0) {
+            $('.top').toggleClass('top--menu_opened');
+        }
+    });
+
+    $(window).scroll(function() {
+
+        if ($(window).scrollTop() > 0) {
+            $('.header').addClass('header--scrolling');
+        }
+        else {
+            $('.header').removeClass('header--scrolling');
+        }
+
+        const wh = $(window).height();
+        const elOffset = $('.price').offset().top;
+        const startAnimPosition = elOffset - (wh - 100);
+        if  ($(window).scrollTop() > startAnimPosition) {
+            $('.price__flex').removeClass('price__flex--hidden');
+        }
+        else {
+            $('.price__flex').addClass('price__flex--hidden');
+        }
     });
 });
